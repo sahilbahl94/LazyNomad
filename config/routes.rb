7a343ee5816efd57183f37e1fda0d60fc501b3c9
Sahil Bahl <sahilbahl94@gmail.com>
2015-12-03 11:42:26 +0100

    front page changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
     root to: "queries#index"
   devise_for :users
   resources :queries, only: [:index]
-  
+
+  get "/home" => "queries#home"
   post "/saved" => "queries#saved"
   get "/near_me" => "queries#near_me"
   get "/show" => "queries#show"
